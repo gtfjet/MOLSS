3c13d5deb4b178415e63a0bb2d45c5048b30e2a4
Dave <gtfjet@gmail.com>
2016-06-12 14:06:54 -0400

    multi-floor wall following

--- molss.c
+++ molss.c
@@ -7,154 +7,200 @@
 
 #define DIMX 460
 #define DIMY 540
+#define DIMZ 4
 
 HDC DC;
-int x, y;
-int M[DIMX][DIMY] = {0};      
-unsigned char R[DIMX][DIMY], G[DIMX][DIMY], B[DIMX][DIMY];
+int x, y, z, count=4, M[DIMX][DIMY][DIMZ] = {0};      
+unsigned char R[DIMX][DIMY][DIMZ], G[DIMX][DIMY][DIMZ], B[DIMX][DIMY][DIMZ];
+
+int mod(int a, int b) {
+    int r = a % b;
+    return r < 0 ? r + b : r;
+}
+
+void makeM() {
+	int i, j, k;
+	for(i=0; i<DIMX; i++) {	
+		for(j=0; j<DIMY; j++) {
+			for(k=0; k<DIMZ; k++) {
+				if (R[i][j][k]==153 && G[i][j][k]==102 && B[i][j][k]==51) {
+					M[i][j][k] = 1;  //walkable=1
+				} else if (R[i][j][k]==255 && G[i][j][k]==255 && B[i][j][k]==0) {
+					M[i][j][k] = 2;  //hole=2
+				} else if (R[i][j][k]==153 && G[i][j][k]==153 && B[i][j][k]==153) {
+					M[i][j][k] = 2;  //hole=2
+				}
+			}
+		}
+	}
+}
+
+void draw() {
+	int i, j, k;
+	COLORREF C;
+	for(k=0; k<DIMZ; k++) {
+		for(j=0; j<DIMY; j++) {
+			for(i=0; i<DIMX; i++) {
+				C = RGB(R[i][j][k], G[i][j][k], B[i][j][k]);
+				SetPixelV(DC,i+DIMX*k,j,C); 
+			}
+		}
+	}
+	
+}
 
 void movein(int h) {
 	int i, j, dx, dy;
 	COLORREF C = RGB(255,255,255);
 	
+	/* Update x and y */
 	if(h==0) {			//head N
-		dx = 0; dy = -3;
+		dx = 0;  dy = -3;
 	} else if(h==1) {	//head NE
-		dx = 3; dy = -3;
+		dx = 3;  dy = -3;
 	} else if(h==2) {	//head E
-		dx = 3; dy = 0;
+		dx = 3;  dy = 0;
 	} else if(h==3) {	//head SE	
-		dx = 3; dy = 3;
+		dx = 3;  dy = 3;
 	} else if(h==4) {	//head S
-		dx = 0; dy = 3;
+		dx = 0;  dy = 3;
 	} else if(h==5) {	//head SW
 		dx = -3; dy = 3;
 	} else if(h==6) {  	//head W
 		dx = -3; dy = 0;
 	} else if(h==7) {	//head NW
 		dx = -3; dy = -3;
+	} else {
+		dx = 0;  dy = 0;
 	}
-	
 	x+=dx; y+=dy;
-		
+
+	/* Color pixels */
 	for(i=-1; i<=1; i++) {
 		for(j=-1; j<=1; j++) {
-			SetPixelV(DC,x+i,y+j,C); 
-		}
-	}
-}
-
-void makeM() {
-	int i, j;
-	for(i=0; i<DIMX; i++) {	
-		for(j=0; j<DIMY; j++) {
-			if (R[i][j]==153 && G[i][j]==102 && B[i][j]==51) {
-				M[i][j] = 1;  //walkable=1
-			}
+			SetPixelV(DC,x+i+DIMX*z,y+j,C); 
 		}
 	}
 }
 
 void walk() {
-	int i, n, h, itWorked;
-	int b[8];
+	int i, j, n, h, b[8], p[]={0,1,2,3,-1,-2,-3}, q[]={7,6,5,0,8,4,1,2,3};
 	
-	/* Initial position and heading */
-	h = 4;	x = 175;  y = 190;
+	/* Initial heading and position */
+	h = 0;	x = 289;  y = 214;	z = 0;
 	
 	while(1) {
+		n = 0;
 		movein(h);
-		for(i=0; i<8; i++) {b[i]=0;}
 		
-		if(M[x][y-3]==1 && h!=4 && M[x+3][y-3] == 0) {	//head N
-			b[0]=1;
-		}
-		if(M[x+3][y-3]==1 && h!=5 && M[x+3][y] == 0) {	//head NE
-			b[1]=1;
-		}
-		if(M[x+3][y]==1 && h!=6 && M[x+3][y+3] == 0) {	//head E
-			b[2]=1;
-		}
-		if(M[x+3][y+3]==1 && h!=7 && M[x][y+3] == 0) {	//head SE	
-			b[3]=1;
-		}
-		if(M[x][y+3]==1 && h!=0 && M[x-3][y+3] == 0) {	//head S
-			b[4]=1;
-		}
-		if(M[x-3][y+3]==1 && h!=1 && M[x-3][y] == 0) {	//head SW
-			b[5]=1;
-		}
-		if(M[x-3][y]==1 && h!=2 && M[x-3][y-3] == 0 ) {  //head W
-			b[6]=1;
-		}
-		if(M[x-3][y-3]==1 && h!=3 && M[x][y-3] == 0) {	//head NW
-			b[7]=1;
-		}
-		
-		n=0;
-		for(i=0; i<8; i++) {n += b[i];}
-		
-		if (n == 0) {
-			h=(h+4)%8;
-		}
-		else if (n == 1) {
-			for(i=0; i<8; i++) {
-				if(b[i] == 1) {
-					h=i;
+		/* Look for a hole */
+		if(count>6) {
+			for(i=-3; i<=3; i+=3) {
+				for(j=-3; j<=3; j+=3) {
+					if(M[x+i][y+j][z] == 2) {
+						h = q[n];
+						if(z==0) {
+							z++;
+						} else if(z==DIMZ) {
+							z--;
+						} else if(M[x+i][y+j][z-1] == 2) {
+							z--;
+						} else {
+							z++;
+						}
+						count = 0;
+						printf("%i\n",z);
+						break;
+					} 
+					n++;
 				}
 			}
 		}
-		else if (n == 2) {
-			itWorked = 0;
-			for(i=0; i<4; i++) {
-				if(b[(h+i)%8] == 1) {
-					h = (h+i)%8;
-					itWorked=1;
-					break;
-				}
+		
+		/* Wall following */
+		if(count>0) {	
+			for(i=0; i<8; i++) {b[i] = 0;}	
+			
+			if(h!=4 && M[x][y-3][z]==1 && M[x+3][y-3][z]==0) {	//head N
+				b[0] = 1;
+			}
+			if(h!=5 && M[x+3][y-3][z]==1 && M[x+3][y][z]==0) {	//head NE
+				b[1] = 1;
+			}
+			if(h!=6 && M[x+3][y][z]==1 && M[x+3][y+3][z]==0) {	//head E
+				b[2] = 1;
+			}
+			if(h!=7 && M[x+3][y+3][z]==1 && M[x][y+3][z]==0) {	//head SE	
+				b[3] = 1;
+			}
+			if(h!=0 && M[x][y+3][z]==1 && M[x-3][y+3][z]==0) {	//head S
+				b[4] = 1;
+			}
+			if(h!=1 && M[x-3][y+3][z]==1 && M[x-3][y][z]==0) {	//head SW
+				b[5] = 1;
+			}
+			if(h!=2 && M[x-3][y][z]==1 && M[x-3][y-3][z]==0) {  //head W
+				b[6] = 1;
+			}
+			if(h!=3 && M[x-3][y-3][z]==1 && M[x][y-3][z]==0) {	//head NW
+				b[7] = 1;
 			}
 			
-			if(itWorked==0) {
-				for(i=0; i<4; i++) {
-					if(b[(h-i)%8 < 0 ? 8-(h-i)%8 : (h-i)%8] == 1) {
-						h = (h-i)%8;
+			for(i=0; i<8; i++) {n += b[i];}
+			
+			if(n == 0) {
+				h = mod(h+4,8);
+			} else if(n == 1) {
+				for(i=0; i<8; i++) {
+					if(b[i] == 1) {
+						h = i;
+						break;
+					}
+				}
+			} else {
+				for(i=0; i<7; i++) {
+					if(b[mod(h+p[i],8)] == 1) {
+						h = mod(h+p[i],8);
 						break;
 					}
 				}
 			}
 		}
-		//printf("%i,%i\n",n,h);
-		Sleep(1);
+		
+		count++;
+		//printf("%i,%i\n",n,h);		
+		Sleep(100);
 	}
 }
 
 void main() {
-	int i, j;
-	COLORREF C;
-	FILE *fp = fopen("Mols-3.ppm","rb");
+	FILE *fp;
+	int i, j, k;
+	char fname[16];
+	system("mode 300,34");
 	DC = GetDC(GetConsoleWindow());
-	system("mode 58,34");
 
-	/* Read header information */
-	for(i=0; i<15; i++) {fgetc(fp);}
+	for(k=0; k<DIMZ; k++) {
+		/* Open ppm */
+		sprintf(fname,"Mols-%i.ppm",k+3);
+		fp = fopen(fname,"rb");
+	
+		/* Read header information */
+		for(i=0; i<15; i++) {fgetc(fp);}
 
-	/* Read image data and print to console */
-	for(j=0; j<DIMY; j++) {
-		for(i=0; i<DIMX; i++) {
-			R[i][j] = fgetc(fp);
-			G[i][j] = fgetc(fp);
-			B[i][j] = fgetc(fp);
-			C = RGB(R[i][j], G[i][j], B[i][j]);
-			SetPixelV(DC,i,j,C); 
+		/* Read image data and print to console */
+		for(j=0; j<DIMY; j++) {
+			for(i=0; i<DIMX; i++) {
+				R[i][j][k] = fgetc(fp);
+				G[i][j][k] = fgetc(fp);
+				B[i][j][k] = fgetc(fp);
+			}
 		}
+		fclose(fp);
 	}
-	fclose(fp);
 	
-	/* Read walkable spaces */
-	makeM();
-	
-	/* Start navigation */
-	walk();
+	/* Create the map and start navigation*/
+	makeM();	draw();		walk();
 	
 	/* End gracefully */
 	ReleaseDC(GetConsoleWindow(),DC);
